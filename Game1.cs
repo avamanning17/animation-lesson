fe3eaae8bcc565fb2eba15801535008b4c4bdda7
Ava Manning <362579849@GoTVDSB.ca>
2025-11-13 09:34:32 -0500

    Add project files.

---
+++ Game1.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace animation_lesson
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+
+        Rectangle window;
+        Rectangle tribbleOrangeRect, tribbleGreyRect;
+
+        Texture2D tribbleOrangeTexture, tribbleGreyTexture;
+
+        Vector2 tribbleOrangeSpeed;
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            window = new Rectangle(0, 0, 800, 600);
+            _graphics.PreferredBackBufferWidth = window.Width;
+            _graphics.PreferredBackBufferHeight = window.Height;
+            _graphics.ApplyChanges();
+
+            tribbleOrangeRect = new Rectangle(300, 10, 100, 100);
+            tribbleOrangeSpeed = new Vector2(2, 1);
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
+
+            tribbleOrangeTexture = Content.Load<Texture2D>("tribbleOrange");
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+
+            tribbleOrangeRect.X += (int) tribbleOrangeSpeed.X;
+            tribbleOrangeRect.Y += (int)tribbleOrangeSpeed.Y;
+            if (tribbleOrangeRect.Right > window.Width || tribbleOrangeRect.Left < 0)
+            {
+                tribbleOrangeSpeed.X *= -1;
+            }
+            if (tribbleOrangeRect.Top < 0 || tribbleOrangeRect.Bottom > window.Height)
+            {
+                tribbleOrangeSpeed.Y *= -1;
+            }
+
+
+                base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.LightPink);
+
+
+            // TODO: Add your drawing code here
+
+            _spriteBatch.Begin();
+
+            _spriteBatch.Draw(tribbleOrangeTexture, tribbleOrangeRect, Color.White);
+
+            _spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,2 @@
+﻿using var game = new animation_lesson.Game1();
+game.Run();
